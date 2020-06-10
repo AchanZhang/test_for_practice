66571b2020f48fbb23fd2fd11db4856c5e041483
张誉川 <zhangyuchuan@evercreative.com.cn>
2020-06-10 10:47:21 +0800

    测试

--- src/main/java/com/achanzhang/free/IntTest.java
+++ src/main/java/com/achanzhang/free/IntTest.java
@@ -6,8 +6,18 @@ package com.achanzhang.free;
  */
 public class IntTest {
     public static void main(String[] args) {
-        int i = 5;
-        int o = 2;
-        System.out.println(i/o);
+//        int i = 5;
+//        int o = 2;
+//        System.out.println(i/o);
+        int i = 22;
+        if (i < 20) {
+            System.out.println("1");
+        } else if (i<25) {
+            System.out.println("2");
+        } else if (i<30) {
+            System.out.println("3");
+        } else {
+            System.out.println("4");
+        }
     }
 }
